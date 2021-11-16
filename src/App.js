1d8d7f5c702a9a3859aac70fef9d4a2ef2454ba3
Gang Zhang <gzhangx@hotmail.com>
2021-11-16 13:51:10 -0500

    'init'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import QrReader from 'react-qr-scanner-ios'
 
 function App() {
+  const [result, setResult] = useState();
+  const [errTxt, setErrTxt] = useState('');
+  const [debugMsg, setDebugMsg] = useState('');
+  const [debugHandleMsg, setDebugHandleMsg] = useState('');
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header">        
+        <QrReader
+          delay={500}
+          facingMode="rear"
+          isIos={true}
+          style={{
+            height: 500,
+            width: 500,
+          }}
+          onError={err => {
+            setErrTxt(err.message);
+          }}
+          setDebugMsg={(msg,msg1) => {
+            if (msg === 'handleWorkerMessage') {
+              setDebugHandleMsg(msg1)
+            } else if (msg === 'checkCount') {
+              setDebugMsg(msg1)
+            } else setDebugMsg(msg)
+            
+          }}
+          onScan={obj => {
+            if (obj) {
+              let text = obj.text;
+              if (typeof obj === 'string') {
+                text = obj;
+              }
+              if (text === ' ') text = '';
+              if (text) {              
+                setResult(text);
+              }
+            }
+          }}
+        />
+        <p>Result={result}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.acccn.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Acccn QR Scanner Test rear-
         </a>
+        <p>debugHandleMsg ${debugHandleMsg}</p>
+        <p>{debugMsg}</p>
+        <p>{errTxt}</p>
       </header>
     </div>
   );
