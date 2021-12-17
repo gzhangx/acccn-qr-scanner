2d5b87ddf54910fbfd16e34a195e96539f451344
Gang Zhang <gzhangx@hotmail.com>
2021-12-16 23:47:37 -0500

    'test'

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -56,7 +56,7 @@ function RegisterForm(props) {
                     if (!values?.count) {
                         errors.count = 'Required';
                     } else {
-                        if (!values.count.match(/^[0-9]+$/)) {
+                        if (!values.count.toString().match(/^[0-9]+$/)) {
                             errors.count = 'Count must be a number';
                         }
                     }
